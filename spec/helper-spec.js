3320957f76a0a9ff96530a192b322bfd8aab71c5
aviatesk <aviatesk@gmail.com>
2019-07-07 19:49:19 +0900

    Update TODOs

---
+++ spec/helper-spec.js
@@ -0,0 +1,10 @@
+describe('Helper methods', () => {
+  // @TODO: Write specs for helper methods
+  // - `repositoryForEditor`
+  //      * Can find a repository in current projects
+  //      * Can find a repository outside current projects
+  //      * Don't cause error when .git repository is deleter
+  //      * Can recognize an newly added .git repository after `initializeRepositoryCache`
+  //      * Time check for each call ?
+  console.log(`Specs for helper methods have not be done yet`);
+});
--- spec/status-bar-integration-spec.js
+++ spec/status-bar-integration-spec.js
@@ -1,4 +1,11 @@
 describe('The integration with Status-Bar package', () => {
   // @TODO: Write specs for the Status-Bar integration
+  // - `StatusBarController` object
+  //      * can show diffs in status-bar
+  //            + show `added`, `modified` and `deleted` lines for a modified file
+  //            + show only `added` lines for an newly file
+  //            + show only `ignored` status for an ignored file
+  //      * can update diffs for switched editor
+  //      * clears diffs for editors under non-.git directory and non editors
   console.log(`Specs for the Status-Bar integration have not be done yet`);
 });
